22cdaef8e8d2037035c32b905498a350f4b80053
Natanael Oliva <natanael.og@gmail.com>
2016-01-08 16:16:43 +0000

    Update GetJWTListener.php

--- Security/Firewall/GetJWTListener.php
+++ Security/Firewall/GetJWTListener.php
@@ -1,7 +1,5 @@
 <?php
-
 namespace Gfreeau\Bundle\GetJWTBundle\Security\Firewall;
-
 use Symfony\Component\Security\Http\Firewall\ListenerInterface;
 use Symfony\Component\Security\Core\SecurityContextInterface;
 use Symfony\Component\Security\Core\Authentication\AuthenticationManagerInterface;
@@ -11,24 +9,22 @@ use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpKernel\Event\GetResponseEvent;
+use Symfony\Component\Debug\Exception\ContextErrorException;
 use Symfony\Component\Security\Core\Exception\AuthenticationException;
 use Symfony\Component\Security\Core\Authentication\Token\UsernamePasswordToken;
 use Symfony\Component\Security\Core\Authentication\Token\TokenInterface;
 use Psr\Log\LoggerInterface;
 use RuntimeException;
 use InvalidArgumentException;
-
 class GetJWTListener implements ListenerInterface
 {
     protected $providerKey;
     protected $options;
     protected $logger;
-
     private $securityContext;
     private $authenticationManager;
     private $successHandler;
     private $failureHandler;
-
     /**
      * @param SecurityContextInterface $securityContext
      * @param AuthenticationManagerInterface $authenticationManager
@@ -44,7 +40,6 @@ class GetJWTListener implements ListenerInterface
         if (empty($providerKey)) {
             throw new InvalidArgumentException('$providerKey must not be empty.');
         }
-
         $this->securityContext = $securityContext;
         $this->authenticationManager = $authenticationManager;
         $this->providerKey = $providerKey;
@@ -57,70 +52,67 @@ class GetJWTListener implements ListenerInterface
         ), $options);
         $this->logger = $logger;
     }
-
     /**
      * {@inheritdoc}
      */
     public function handle(GetResponseEvent $event)
     {
         $request = $event->getRequest();
-
-        if ($this->options['post_only'] && !$request->isMethod('POST')) {
-            $event->setResponse(new JsonResponse('invalid method', 405));
-            return;
+        if ($request->headers->get('content_type') == 'application/json') {
+            $content = $request->getContent();
+            $params = (!empty($content)) ? json_decode($content, true) : array();
+            try {
+                $username = trim($params[$this->options['username_parameter']]);
+                $password = trim($params[$this->options['password_parameter']]);
+            } catch (ContextErrorException $e) {
+                throw new AuthenticationException('JWT request failed: JSON data invalid.');
+            }
         }
-
-        if ($this->options['post_only']) {
-            $username = trim($request->request->get($this->options['username_parameter'], null, true));
-            $password = $request->request->get($this->options['password_parameter'], null, true);
-        } else {
-            $username = trim($request->get($this->options['username_parameter'], null, true));
-            $password = $request->get($this->options['password_parameter'], null, true);
+        else {
+            if ($this->options['post_only'] && !$request->isMethod('POST')) {
+                $event->setResponse(new JsonResponse('invalid method', 405));
+                return;
+            }
+            if ($this->options['post_only']) {
+                $username = trim($request->request->get($this->options['username_parameter'], null, true));
+                $password = $request->request->get($this->options['password_parameter'], null, true);
+            } else {
+                $username = trim($request->get($this->options['username_parameter'], null, true));
+                $password = $request->get($this->options['password_parameter'], null, true);
+            }
         }
-
         try {
             $token = $this->authenticationManager->authenticate(new UsernamePasswordToken($username, $password, $this->providerKey));
             $this->securityContext->setToken($token);
             $response = $this->onSuccess($event, $request, $token);
-
         } catch (AuthenticationException $e) {
             if (null == $this->failureHandler) {
                 throw $e;
             }
-
             $response = $this->onFailure($event, $request, $e);
         }
-
         $event->setResponse($response);
     }
-
     protected function onSuccess(GetResponseEvent $event, Request $request, TokenInterface $token)
     {
         if (null !== $this->logger) {
             $this->logger->info(sprintf('User "%s" has retrieved a JWT', $token->getUsername()));
         }
-
         $response = $this->successHandler->onAuthenticationSuccess($request, $token);
-
         if (!$response instanceof Response) {
             throw new \RuntimeException('Authentication Success Handler did not return a Response.');
         }
-
         return $response;
     }
-
     protected function onFailure(GetResponseEvent $event, Request $request, AuthenticationException $failed)
     {
         if (null !== $this->logger) {
             $this->logger->info(sprintf('JWT request failed: %s', $failed->getMessage()));
         }
-
         $response = $this->failureHandler->onAuthenticationFailure($request, $failed);
-
         if (!$response instanceof Response) {
             throw new \RuntimeException('Authentication Failure Handler did not return a Response.');
         }
-
         return $response;
     }
 }
